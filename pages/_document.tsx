7734debe4d51cb9d43d14ffe89e34cde14591acd
imnyang <nyang.nyanglab@gmail.com>
2023-05-14 12:48:16 +0900

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,7 @@ export default function Document() {
   return (
     !
     <Html lang="en">
-      <Head>
-        <title>👀 By 🔈</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
