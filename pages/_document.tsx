7054b4f20821b964603edee2f8c2074ae726f408
imnyang <nyang.nyanglab@gmail.com>
2023-05-14 12:27:43 +0900

    testing tittle

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
+    !
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>👀 By 🔈</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
