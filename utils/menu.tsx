99bccb7bb14477eea4c20804a6965ba7f2623ce5
imnyang <nyang.nyanglab@gmail.com>
2023-06-09 21:36:15 +0900

    Super Bug Fix

--- utils/menu.tsx
+++ utils/menu.tsx
@@ -18,7 +18,7 @@ const TableComponent: React.FC<TableComponentProps> = ({ data }) => {
   );
 
   return (
-    <table>
+    <table className="rounded-table"> {/* 둥근 표를 위한 CSS 클래스 적용 */}
       <thead>
         <tr>
           <th>Category</th>
@@ -37,18 +37,19 @@ const TableComponent: React.FC<TableComponentProps> = ({ data }) => {
               {sortedMenus.map(([menu, price], index) => (
                 <tr key={`${category}-${menu}`}>
                   {index === 0 && (
-                    <td rowSpan={sortedMenus.length}>{category}</td>
+                    <td rowSpan={sortedMenus.length} style={{ verticalAlign: 'top' }}>{category}</td>
                   )}
                   <td>{menu}</td>
                   <td>{price}</td>
                 </tr>
               ))}
+              <tr style={{ height: '20px' }}></tr> {/* 한 종류가 지나가면 표가 한 줄 띄워질 수 있도록 빈 줄 추가 */}
             </React.Fragment>
           );
         })}
       </tbody>
     </table>
   );
-}; 
+};
 
 export default TableComponent;
